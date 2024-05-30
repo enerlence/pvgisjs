0e85fb8bafc6da5d4f18bafd3ebfe2989ca7e80e
psanchezmvelarde <56727129+pabloiea1995@users.noreply.github.com>
2024-05-30 17:20:14 +0200

    updated pvgis radiation databases enum

--- src/model/pvgisRadiationDatabases.ts
+++ src/model/pvgisRadiationDatabases.ts
@@ -1,13 +1,15 @@
 /**
- * Name of the radiation database (DB): "PVGIS-SARAH" for Europe, Africa and Asia or "PVGIS-NSRDB" for the Americas
+ * Name of the radiation database (DB): "PVGIS-SARAH2" for Europe, Africa and Asia or "PVGIS-NSRDB" for the Americas
  * between 60°N and 20°S,  "PVGIS-ERA5" and "PVGIS-COSMO" for Europe (including high-latitudes), and "PVGIS-CMSAF"
- * for Europe and Africa (will be deprecated).The default DBs are PVGIS-SARAH, PVGIS-NSRDB and PVGIS-ERA5
+ * for Europe and Africa (will be deprecated).The default DBs are PVGIS-SARAH2, PVGIS-NSRDB and PVGIS-ERA5
  *  based on the chosen location.
  */
-export enum PVGISRadiationDatabase {
-  PVGIS_SARAH = 'PVGIS-SARAH',
-  PVGIS_NSRDB = 'PVGIS-NSRDB',
-  PVGIS_ERA5 = 'PVGIS-ERA5',
-  PVGIS_COSMO = 'PVGIS-COSMO',
-  PVGIS_CMSAF = 'PVGIS-CMSAF',
+export declare enum PVGISRadiationDatabase {
+  PVGIS_SARAH = "PVGIS-SARAH",
+  PVGIS_SARAH2 = "PVGIS-SARAH2",
+  PVGIS_NSRDB = "PVGIS-NSRDB",
+  PVGIS_ERA5 = "PVGIS-ERA5",
+  //removed from PVGIS 5.2 as per their documentation
+  // PVGIS_COSMO = "PVGIS-COSMO",
+  // PVGIS_CMSAF = "PVGIS-CMSAF"
 }
